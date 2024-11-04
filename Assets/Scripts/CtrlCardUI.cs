5456e83ea331a0714efa10cf8a08d41d5dae90f2
sihuop0823 <sihuop0823@gmail.com>
2024-11-04 19:34:58 +0900

    버튼 눌렀을 때 반응
    아 -- 홍보 존나 힘들다

---
+++ Assets/Scripts/CtrlCardUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CtrlCardUI : MonoBehaviour
+{
+    public GameObject CtrlPanel;
+    public Button CtrlCrtBtn;
+
+    private void Start()
+    {
+        CtrlPanel.SetActive(false);
+    }
+    /*private bool state;
+
+    void Start()
+    {
+        state = true;
+    }*/
+
+    public void OnClick()
+    {
+        CtrlPanel.SetActive(true);
+    }
+
+}// void 업데이트 추가하려면 여기 중괄호 지우면 됨.
+    /*void Update()
+    {
+        if (CardCrtBtn)
+        {
+            Debug.Log("마우스 입력 받았슴");
+            if (state == true)
+            {
+                CtrlPanel.SetActive(false);
+                Debug.Log("disapper");
+                state = false;
+            }
+
+            else
+            {
+                CtrlPanel.SetActive(true);
+                Debug.Log("apper");
+                state = true;
+            }
+        }
+    }
+}*/
