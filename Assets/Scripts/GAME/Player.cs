f99cdbfc335631a2586ac03ad24dc01977be78cb
sihuop0823 <sihuop0823@gmail.com>
2024-11-28 23:14:27 +0900

    2024 11 28
    Remake

---
+++ Assets/Scripts/GAME/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+    [Header("Border")]
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+    // gameobject 선언을 하고 Border에 tag를 달아서 CompareTag로 하는게 보기에도 좋았을까?
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        if((isTouchRight && x == 1) || (isTouchLeft && x == -1))
+            x = 0; 
+        float y = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && y == 1) || (isTouchBottom && y == -1))
+            y = 0;
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(x, y, 0)* Speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true; 
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
+    }
+}
