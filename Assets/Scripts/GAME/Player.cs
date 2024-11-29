3bf50cd992e91c378733cfcf95ed68e2a8de0b84
sihuop0823 <sihuop0823@gmail.com>
2024-11-30 00:08:29 +0900

    2024 11 30
    매우 많은 작업을 한. 근데 졸라 많이 날려먹은

--- Assets/Scripts/GAME/Player.cs
+++ Assets/Scripts/GAME/Player.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player")]
     public float Speed;
+
+    [Header("Bullet")]
+    public float maxShotDelay;
+    public float curShotDelay;
+    public float power;
 
     [Header("Border")]
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchLeft;
     public bool isTouchRight;
+
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
     //이렇게 일일히 하지 않고 isTouchBorder 선언을 하고 gameobject 선언을 하고 Border에 tag를 달아서 CompareTag로 하는게 보기에도 좋았을까?
 
     Animator anim;
@@ -22,23 +31,79 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    void Move()
+    {
         float x = Input.GetAxisRaw("Horizontal");
-        if((isTouchRight && x == 1) || (isTouchLeft && x == -1))
-            x = 0; 
+        if ((isTouchRight && x == 1) || (isTouchLeft && x == -1))
+            x = 0;
         float y = Input.GetAxisRaw("Vertical");
         if ((isTouchTop && y == 1) || (isTouchBottom && y == -1))
             y = 0;
 
         Vector3 curPos = transform.position;
-        Vector3 nextPos = new Vector3(x, y, 0)* Speed * Time.deltaTime;
+        Vector3 nextPos = new Vector3(x, y, 0) * Speed * Time.deltaTime;
 
         transform.position = curPos + nextPos;
 
 
-        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
         {
             anim.SetInteger("Input", (int)x);
         }
+    }
+
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1"))
+            return;
+
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        switch (power)
+        {
+            case 1:
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.25f, transform.rotation);
+                GameObject bulletM = Instantiate(bulletObjA, transform.position, transform.rotation);
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.25f, transform.rotation);
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidM = bulletM.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidM.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 4:
+                GameObject bulletFin = Instantiate(bulletObjB, transform.position, transform.rotation);
+                Rigidbody2D rigidFin = bulletFin.GetComponent<Rigidbody2D>();
+                rigidFin.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+                break;
+        }
+
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
